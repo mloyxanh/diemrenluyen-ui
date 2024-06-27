e10abf762f79456ddd6bdb3f7542cb4cea54c5a2
mloyxanh <mloyxanh270898@gmail.com>
2024-06-27 22:23:17 +0700

    change title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>XANH</code>
+          <code>MUN</code>
         </p>
         <a
           className="App-link"
